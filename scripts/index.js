ab788f86d49b490b3c39700da5aadee87eb32bae
BoinBoinMango <39943769+BoinBoinMango@users.noreply.github.com>
2022-03-31 19:14:08 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'April 1, 2022',
-  name: 'Mahal kooooo'
+  birthdate: 'March 31, 2022',
+  name: 'Mahal kooooo, I LOVE YOU'
 };
 
 function hideEverything() {
