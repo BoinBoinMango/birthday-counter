cbb45d7aaff745513dd38d1ba9b120ab3f6f046a
BoinBoinMango <39943769+BoinBoinMango@users.noreply.github.com>
2022-03-31 19:13:12 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'April 1, 2022',
+  name: 'Mahal kooooo'
 };
 
 function hideEverything() {
